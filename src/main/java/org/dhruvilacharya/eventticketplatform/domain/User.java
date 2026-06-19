a66b238e972388977772fc3b90708ce76c3465c7
dhruvilacharya <dhruvilacharya3007@gmail.com>
2026-06-19 18:25:21 +0530

    added all entities and equals and hashcode

--- src/main/java/org/dhruvilacharya/eventticketplatform/domain/User.java
+++ src/main/java/org/dhruvilacharya/eventticketplatform/domain/User.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -49,7 +50,7 @@ public class User {
     )
     private List<Event> staffingEvents = new ArrayList<>();
 
-    
+
     @CreatedDate
     @Column(name = "created_at", updatable = false, nullable = false)
     private LocalDateTime createdAt;
@@ -58,4 +59,15 @@ public class User {
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(createdAt, user.createdAt) && Objects.equals(updatedAt, user.updatedAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, createdAt, updatedAt);
+    }
 }
