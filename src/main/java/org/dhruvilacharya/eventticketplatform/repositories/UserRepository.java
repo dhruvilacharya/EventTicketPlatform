dffb303e180d7785419c700b7d9be6d51add1ac5
dhruvilacharya <dhruvilacharya3007@gmail.com>
2026-06-19 20:09:40 +0530

    Jpa audit and Jwt added

---
+++ src/main/java/org/dhruvilacharya/eventticketplatform/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package org.dhruvilacharya.eventticketplatform.repositories;
+
+import org.dhruvilacharya.eventticketplatform.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
